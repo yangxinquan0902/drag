334f267702a1db235dbf12fac556f7152713c210
yxq <1501684012@qq.com>
2017-11-09 10:43:14 +0800

    new

--- drag.js
+++ drag.js
@@ -3,6 +3,7 @@ function drag(id){
 	var obj = document.getElementById(id);
 	var disX = 0;
 	var disY = 0;
+	var wu = null;
 	obj.onmousedown = function(ev){
 		var l = ev.clientX - obj.offsetLeft;
 		var t = ev.clientY - obj.offsetTop;
