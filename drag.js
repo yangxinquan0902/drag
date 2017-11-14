3eebc055d5a09cfbbe7956e5a18db898b40c720a
yxq <1501684012@qq.com>
2017-11-14 15:20:24 +0800

    drag

--- drag.js
+++ drag.js
@@ -1,11 +1,26 @@
 
-function drag(id){
-	var obj = document.getElementById(id);
-	var disX = 0;
-	var disY = 0;
-	var wu = null;
-	obj.onmousedown = function(ev){
-		var l = ev.clientX - obj.offsetLeft;
-		var t = ev.clientY - obj.offsetTop;
-	};
+window.onload = function(){
+	
+	var oApp = document.querySelector('.app');
+	
+	function drag(ele){
+		var obj = ele;
+		
+		obj.onmousedown = function(ev){
+			var disX = ev.clientX - obj.getBoundingClientRect().left;
+			var disY = ev.clientY - obj.getBoundingClientRect().top;
+			document.onmousemove = function(ev){
+				var l = ev.clientX - disX;
+				var t = ev.clientY - disY;
+				obj.style.left = l+'px';
+				obj.style.top = t+'px';
+			};
+			
+			document.onmouseup= function(){
+				document.onmouseup = document.onmousemove = null
+			};
+			return false;
+		};
+	}
+	drag(oApp);
 }
